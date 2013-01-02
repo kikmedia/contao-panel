47afbab24a6d4f27a57c8206c16cd0a49257f656
Carolina M. Koehn <office@kikmedia.de>
2013-01-02 13:59:38 +0100

    Autoloader added

---
+++ system/modules/sliding-panel/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Sliding-panel
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'moo_slidepanel' => 'system/modules/sliding-panel/templates',
+));
